93077c152c9e7888d5bb86bb9b2ae6076abd23fe
MiguelRodrigues <miguel.silveira.rodrigues@escola.pr.gov.br>
2026-03-25 18:43:32 +0000

    mhfh

---
+++ temperatura.js
@@ -0,0 +1,8 @@
+//Conversor de temperaturas
+const cidade = "Curitiba";
+let tempCelcius = 18.5;
+
+const tempFahrenheit = tempCelcius * (9/5) + 32;
+const tempkelvin = tempCelcius + 273.15;
+
+console.log(`A temperatura de ${cidade} está ${tempCelcius} Cº`)
