51d2c78265b051da7eac2ba4acf9a47ad2f243e4
MiguelRodrigues <miguel.silveira.rodrigues@escola.pr.gov.br>
2026-04-14 17:34:39 +0000

    versao2

---
+++ nivel.js
@@ -0,0 +1,9 @@
+function subirDeNivel(nivelAtual) {
+    return nivelAtual +1;
+};
+
+let meuNivel = 10;
+let nivelAtualizado = subirDeNivel(meuNivel); // somando nivel atual + 1
+//
+
+console.log("Seu nível é: " + nivelAtualizado);
